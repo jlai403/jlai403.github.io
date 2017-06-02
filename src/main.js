f0fe6353db050fa0d2426a84c1544f3035beb864
Joey <joey@jlai.ca>
2017-06-02 16:19:37 -0600

    added font-awesome to npm

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// client npm packages
+import 'font-awesome/css/font-awesome.css'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
