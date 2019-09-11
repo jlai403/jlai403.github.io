0c5639be3a7dcc82a69826c0fcdf5fdd89be502e
Joey <j.lai@live.ca>
2019-09-11 16:00:48 -0400

    migrated initial Vue views to React

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+import './index.scss';
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
