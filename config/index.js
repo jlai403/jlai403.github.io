e03a90726452e3e297a5d8fd4a7b55bc11fa70b2
Joey <j.lai@live.ca>
2019-09-11 17:08:45 -0400

    Removed Vue

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var env = process.env.NODE_ENV
-if (env === undefined || env === null) {
-  throw 'Error determining which config file to use, is NODE_ENV defined?'
-}
-
-// Defaults prod config/environment variables and overriden in dev/test
-module.exports = require(`./${env}.config`)
