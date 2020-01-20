883abf054833cb894605952b77c114eab472c5a7
Joey <j.lai@live.ca>
2020-01-19 23:14:52 -0700

    added geo api and switched router to hash

--- src/components/MainLayout/About.js
+++ src/components/MainLayout/About.js
@@ -5,35 +5,65 @@ import './About.scss';
 import 'font-awesome/css/font-awesome.css'
 
 
-function About() {
-  let yearsExperience = new Date().getFullYear() - 2011
 
-  return (
-    <div id="about" style={{minHeight: '100vh'}}>
-      <div className="container">
-        <div className="intro row">
-          <div className="hello">Hello, my name is</div>
-          <div className="name">Joey Lai</div>
-          <div className="title">and I build products</div>
-        </div>
+async function getGeoData() {
+  let response = await fetch('https://geolocation-db.com/json/')
+  return await response.json()
+}
 
-        <div className="bio row">
-          <p>
-            Some paragraph here
-          </p>
-        </div>
+class About extends React.Component {
 
-        <div className="row">
-          <Link href="" to="skills" spy={true} smooth={true} offset={-65}>
-            <div className="bouncing learn-more">
-              Learn More <br/>
-              <i className="fa fa-chevron-down"></i>
-            </div>
-          </Link>
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  async componentDidMount() {
+    console.log(`TEST VAR: ${process.env.TEST_VAR}`)
+    let yearsExperience = new Date().getFullYear() - 2011
+    let geoData = await getGeoData()
+
+
+    console.log(geoData)
+    this.setState({
+      yearsExperience: yearsExperience,
+      vistorCountry: geoData.country,
+      vistorState: geoData.state,
+      vistorCity: geoData.city,
+      visitorPostal: geoData.postal,
+      vistorLat: geoData.latitude,
+      vistorLong: geoData.longitude
+    })
+  }
+
+  render() {
+    return (
+      <div id="about" style={{minHeight: '100vh'}}>
+        <div className="container">
+          <div className="intro row">
+            <div className="hello">Hello, my name is</div>
+            <div className="name">Joey Lai</div>
+            <div className="title">and I build products</div>
+          </div>
+
+          <div className="bio row">
+            <p>
+              Some paragraph here
+            </p>
+          </div>
+
+          <div className="row">
+            <Link href="" to="skills" spy={true} smooth={true} offset={-65}>
+              <div className="bouncing learn-more">
+                Learn More <br/>
+                <i className="fa fa-chevron-down"></i>
+              </div>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </div>)
+  }
+
 }
 
 export default About;
