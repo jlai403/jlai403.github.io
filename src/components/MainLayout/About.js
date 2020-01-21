ebc62228aa8a46e303ef155d61a4db7fbdd070f2
Joey <j.lai@live.ca>
2020-01-20 22:48:55 -0700

    added weather api

--- src/components/MainLayout/About.js
+++ src/components/MainLayout/About.js
@@ -11,6 +11,30 @@ async function getGeoData() {
   return await response.json()
 }
 
+async function getWeatherData(lat, long) {
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6961a843d1a2d12f6cc9f4668124a762`)
+  return await response.json()
+}
+
+function getWeatherText(weatherCode) {
+  switch (weatherCode.toString()[0]) {
+    case '2':
+      return `Looks like there's thunderstorms currently`
+    case '3':
+      return `Looks like there's some drizzlies currently`
+    case '5':
+      return `Looks like it's raining currently`
+    case '6':
+      return `Looks like it's snowing currently`
+    case '7':
+      return `Its clear skies currently`
+    case '8':
+      return `Its currently cloudy`
+    default:
+      return 'Hi'
+    }
+}
+
 class About extends React.Component {
 
   constructor(props) {
@@ -19,20 +43,23 @@ class About extends React.Component {
   }
 
   async componentDidMount() {
-    console.log(`TEST VAR: ${process.env.TEST_VAR}`)
-    let yearsExperience = new Date().getFullYear() - 2011
+    let currentTime = new Date()
     let geoData = await getGeoData()
-
+    let weatherData = await getWeatherData(geoData.latitude, geoData.longitude)
 
     console.log(geoData)
     this.setState({
-      yearsExperience: yearsExperience,
+      currentTimeOfDay: currentTime.getHours() < 12 ? 'Morning' : currentTime.getHours() < 16 ? 'Afternoon' : currentTime.getHours() < 20 ? 'Evening' : 'Night',
+      yearsExperience: currentTime.getFullYear() - 2011,
       vistorCountry: geoData.country,
       vistorState: geoData.state,
       vistorCity: geoData.city,
       visitorPostal: geoData.postal,
       vistorLat: geoData.latitude,
-      vistorLong: geoData.longitude
+      vistorLong: geoData.longitude,
+      weatherText: getWeatherText(weatherData.weather[0].id),
+      city: geoData.city,
+      state: geoData.state
     })
   }
 
@@ -48,7 +75,8 @@ class About extends React.Component {
 
           <div className="bio row">
             <p>
-              Some paragraph here
+              Good {this.state.currentTimeOfDay}, <br/><br/>
+              {this.state.weatherText} in {this.state.city}, {this.state.state}
             </p>
           </div>
 
