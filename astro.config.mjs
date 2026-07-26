332bc149f43f11bca2114fd85ba561f29b7b33cc
joey <3282127+jlai403@users.noreply.github.com>
2026-07-26 13:52:05 -0600

    chore: scaffold astro + svelte project

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  server: { port: 3000 },
+  output: 'static',
+});
